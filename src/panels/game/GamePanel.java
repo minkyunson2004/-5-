208d94fe9caa54e703a0ce8de79cf2ddf669f4d0
손민균 <sonmingyun@sonmingyun-ui-MacBookPro.local>
2024-05-19 23:49:01 +0900

    commit

---
+++ src/panels/game/GamePanel.java
@@ -0,0 +1,15 @@
+package panels.game;
+
+import frame.MainFrame;
+
+public class GamePanel {
+    //MainFrame
+    MainFrame superFrame;
+
+    //GamePanel Component
+
+    //initialize
+    public GamePanel(MainFrame superFrame) {
+        this.superFrame = superFrame;
+    }
+}
